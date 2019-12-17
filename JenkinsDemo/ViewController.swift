aefb6647a6e7f60f634653eb47eaf9c0041ddc7f
Martynas Gavelis <martynas.gavelis@telesoftas.com>
2019-12-17 23:11:58 +0200

    Add xCode project

---
+++ JenkinsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by Martynas Gavelis on 2019-12-17.
+//  Copyright © 2019 Martynas Gavelis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
